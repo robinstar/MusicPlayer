994c30a25a87b8451eedb13c53e6e6e50db36e72
robinstar <liuxing-ruyu@163.com>
2015-07-01 17:24:49 +0800

    Initiate the project.

---
+++ src/com/robin/music/AudioMediaBrowserActivity.java
@@ -0,0 +1,15 @@
+package com.robin.music;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AudioMediaBrowserActivity extends Activity {
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_media_browser);
+    }
+
+}
